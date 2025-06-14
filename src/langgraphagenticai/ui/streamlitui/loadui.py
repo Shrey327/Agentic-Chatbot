5156905b5cae4ee2606e564e8b38475e45b097df
Shrey327 <shreyansh.pathak273@gmail.com>
2025-06-14 17:08:01 +0530

    x

--- src/langgraphagenticai/ui/streamlitui/loadui.py
+++ src/langgraphagenticai/ui/streamlitui/loadui.py
@@ -9,9 +9,8 @@ class LoadStreamlitUI:
         self.user_controls={}
 
     def load_streamlit_ui(self):
-        st.set_page_config(page_title="X" + self.config.get_page_title(), layout="wide")
-        st.header("x" + self.config.get_page_title())
-
+        st.set_page_config(page_title=self.config.get_page_title(), layout="wide")
+        st.header(self.config.get_page_title())
 
         with st.sidebar:
 
