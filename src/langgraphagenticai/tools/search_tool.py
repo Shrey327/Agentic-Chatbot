72f32a4f8e573020e47c2c284cf0e560e0b447c3
Shrey327 <shreyansh.pathak273@gmail.com>
2025-06-26 18:42:15 +0530

    updates

--- src/langgraphagenticai/tools/search_tool.py
+++ src/langgraphagenticai/tools/search_tool.py
@@ -1,8 +1,6 @@
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langgraph.prebuilt import ToolNode
 from langchain_community.tools import ArxivQueryRun
-from langchain_community.tools.wikipedia.tool import WikipediaQueryRun
-from langchain_community.utilities.wikipedia import WikipediaAPIWrapper
 from langchain_community.utilities import ArxivAPIWrapper
 def get_tools():
     """
@@ -10,10 +8,8 @@ def get_tools():
     """
     api_wrapper_arxiv=ArxivAPIWrapper(top_k_results=2,doc_content_chars_max=500)
     arxiv=ArxivQueryRun(api_wrapper=api_wrapper_arxiv)
-    api_wrapper_wiki=WikipediaAPIWrapper(top_k_results=2,doc_content_chars_max=500)
-    wiki=WikipediaQueryRun(api_wrapper=api_wrapper_wiki)
     tavily = TavilySearchResults(max_results=2)
-    tools = [arxiv,wiki,tavily]
+    tools = [arxiv,tavily]
     return tools
 
 
